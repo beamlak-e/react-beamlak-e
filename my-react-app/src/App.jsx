268674ed2b7877ee85eb30a8b439efd63e95cfdf
Beamlak E <b.d.eskender@wust.edu>
2025-11-21 03:10:39 -0600

    handlClick

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -13,6 +13,7 @@ function Square({ value, onSquareClick }) {
 }
 
 function Board() {
+  const[xIsNext, setXIsNext] = useState(true); 
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
