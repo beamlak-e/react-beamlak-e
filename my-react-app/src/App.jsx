067db9094d8fd35d6153048e995e380b744d6ef5
Beamlak E <b.d.eskender@wust.edu>
2025-11-21 01:19:30 -0600

    Added the other squares

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,8 +3,21 @@ import './App.css';
 function Square() {
   return (
     <>
+    <div className="board-row">
     <button className="square">X</button>
     <button className="square">X</button>
+    <button className="square">X</button>
+     </div>
+     <div className="board-row">
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+     </div>
+    <div className="board-row">
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+     </div>
     </>
   );
 }
