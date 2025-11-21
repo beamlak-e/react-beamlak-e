53f29c7e6877582742e249c12813ff735971baf0
Beamlak E <b.d.eskender@wust.edu>
2025-11-21 03:23:31 -0600

    working calcuate winner square

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -17,7 +17,7 @@ function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -29,7 +29,13 @@ function Board() {
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
-
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
   return (
     <>
       <div className="board-row">
